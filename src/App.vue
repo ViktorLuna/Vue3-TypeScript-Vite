3b5632ff28662d52ef8a8f74bbd2cb5d5c82e0d8
Jovi Joshua Tania <jovijoshuatania027@gmail.com>
2024-05-24 15:27:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="App">
+		<h1 style="font-size: 77px;">hello there, Human!.</h1>
+	</div>
+	
+	<router-view/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'App',
+	components: {}
+});
+</script>
+
+<style>
+
+</style>
